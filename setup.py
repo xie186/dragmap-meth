f5e3ccf6499c4302b0ac563e4e947589596f91bf
xie186 <xie186@purdue.edu>
2021-12-18 15:53:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='dragmap-meth',
       install_requires=install_requires,
       long_description=open('README.md').read(),
       classifiers=[
-      'Topic :: Scientific/Engineering :: Bio-Informatics',
+      'Topic :: Scientific/Engineering :: Bioinformatics', 
       'Programming Language :: Python :: 2',
       'Programming Language :: Python :: 3'
       ],
